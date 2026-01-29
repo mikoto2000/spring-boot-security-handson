676ff1947c544c4f410e8bc71809e80697dd2ee4
mikoto2000 <mikoto2000@gmail.com>
2026-01-29 13:44:10 +0000

    DB の初期データ作成。

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO USERS (username, password, enabled)
+VALUES
+(
+  'mikoto2000',
+  -- "{bcrypt}$2a$10$0OsB8/8crrUzT9O8VNJF.uF2sB1c7tpvqJ/COY0Hm9qtoCETRa1cC" = "password"
+  '{bcrypt}$2a$10$0OsB8/8crrUzT9O8VNJF.uF2sB1c7tpvqJ/COY0Hm9qtoCETRa1cC',
+  true
+),
+(
+  'mikoto2001',
+  -- "{bcrypt}$2a$10$0OsB8/8crrUzT9O8VNJF.uF2sB1c7tpvqJ/COY0Hm9qtoCETRa1cC" = "password"
+  '{bcrypt}$2a$10$0OsB8/8crrUzT9O8VNJF.uF2sB1c7tpvqJ/COY0Hm9qtoCETRa1cC',
+  true
+);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE USERS (
+  username VARCHAR(50) PRIMARY KEY,
+  password VARCHAR(255) NOT NULL,
+  enabled BOOLEAN NOT NULL
+);
